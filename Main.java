49650ad9f58056c4e10880aaca10647c423478fb
nonydev27 <karldjansi123@gmail.com>
2026-01-15 21:14:13 +0000

    added scanner

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args){
+
+        //prompt user to enter his/her name
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
         System.out.println("Hello, World!");
+
     }
 }
