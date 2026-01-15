86d1d026045f50da5c29d92dd1d9496ce6b6bede
nonydev27 <karldjansi123@gmail.com>
2026-01-15 21:06:06 +0000

    init changes

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(string[] args){
-        println("Hello, World!");
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
     }
 }
