9714741e559565c210a6a7dece0033fa5d239a7e
nonydev27 <karldjansi123@gmail.com>
2026-01-15 21:56:54 +0000

    added scanner

--- Main.java
+++ Main.java
@@ -1,10 +1,8 @@
-public class Main {
+public class Main{
     public static void main(String[] args){
-
-        //prompt user to enter his/her name
-        int myAge = 30;
-        System.out.println("My age is: " + myAge);
-        System.out.println("Hello, World!");
-
+        
+        //program will prompt user to enter their name
+        System.out.println("Please enter your name:");
     }
 }
+
