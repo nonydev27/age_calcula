71856ce348ee481b615c845fea209694005690fa
nonydev27 <karldjansi123@gmail.com>
2026-01-20 20:48:53 +0000

    ...

--- Main.java
+++ Main.java
@@ -1,8 +1,37 @@
+// import java.util.Date;
+// import java.awt.*;
+
 public class Main{
     public static void main(String[] args){
-        
-        //program will prompt user to enter their name
-        System.out.println("Please enter your name:");
-    }
+        // System.out.println("Hello");
+        // Date now = new Date();
+        // System.out.println(now);
+
+        //not working
+
+        // Point p1 = new Point(x:1, y:1);
+        // Point p2 = p1;
+
+        // System.out.println(p2);
+
+        String message = "Hello World" + "!!";
+    
+        System.out.println(message);
+
+        int myNum = 13;
+        char myLetter = 'D';
+        float deci = 23.45f;
+        boolean myBool = false;
+
+        System.out.println(myLetter);
+        System.out.println(myNum);
+        System.out.println(deci);
+        System.out.println(myBool);
+
+            String x = "water";
+            String y = "kool-aid";
+
+    };
 }
+
 
