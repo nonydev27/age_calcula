dc557d3203ef59eeeb150f7920d8fb7a417320f0
nonydev27 <karldjansi123@gmail.com>
2026-01-21 23:44:45 +0000

    init commit

--- assignment.java
+++ assignment.java
@@ -1,5 +1,6 @@
 public class assignment{
     public static void main(String[] args) {
+        System.out.println("This program is to find the average of 10 figures, find the std deviation and then the factorial");
         
     }
 }
