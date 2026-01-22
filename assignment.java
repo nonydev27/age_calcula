674e2c3e0b80aa3ea32de60a66aac14e884ddac6
nonydev27 <karldjansi123@gmail.com>
2026-01-22 15:00:37 +0000

    init commit

--- assignment.java
+++ assignment.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class assignment {
     public static void main(String[] args) {
-        System.out.println("This program is to find the average of 10 figures, find the std deviation and then the factorial");
+        System.out.println("This program is to find the average of 10 figures, find the standard deviation and then the factorial");
         
         Scanner scanner = new Scanner(System.in);
         double[] numbers = new double[10];
